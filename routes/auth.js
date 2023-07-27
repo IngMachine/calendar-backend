3d55d22e820254ebbd89342c9623a1d02e29376e
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-27 09:40:57 -0500

    Implementacion de las validaciones de cada una de las peticione se utiliza un custom middleware para hacer la validacion de cada una de las validaciones

--- routes/auth.js
+++ routes/auth.js
@@ -4,18 +4,42 @@
  */
 
 const { Router } = require('express');
-const router = Router();
+const { check } = require('express-validator')
 
+const router = Router();
 
 const {
     createUser,
     loginUser,
     reNewToken
 } = require('../controllers/auth')
+const {fieldsValidators} = require("../middleware/fields-validators");
 
-router.post('/new', createUser );
+router.post(
+    '/new',
+    [
+        // middleware
+        check('name', 'The name is required and cannot be empty').not().isEmpty(),
+        check('email', 'The email is required').not().isEmpty(),
+        check('email', 'The email does not comply with the format').isEmail(),
+        check('password', 'The password is required').not().isEmpty(),
+        check('password', 'The password must be greater than or equal to 6 characters.').isLength({min: 6}),
+        fieldsValidators
+    ],
+    createUser
+);
 
-router.post('/', loginUser );
+router.post(
+    '/',
+    [
+        check('email', 'The email is required').not().isEmpty(),
+        check('email', 'The email does not comply with the format').isEmail(),
+        check('password', 'The password is required').not().isEmpty(),
+        check('password', 'The password must be greater than or equal to 6 characters.').isLength({min: 6}),
+        fieldsValidators
+    ],
+    loginUser
+);
 
 router.get('/renew', reNewToken)
 
