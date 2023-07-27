ddbbb78707f8b4a33c4ff2ebd157a9e4a98d91c3
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-27 09:11:35 -0500

    Obtener los campos del formulario por el body, enviar los diferente status de la respuesta

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,31 @@
-const {response} = require('express');
-const createUser = (req, res = response) => {
-    res.json({
-        ok: true
+const {request, response, json} = require('express');
+const createUser = (req = request, res = response) => {
+    const { name, email, password } = req.body;
+
+    if( name.length < 5 ) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El nombre debe de ser de 5 letras'
+        });
+    }
+
+    res.status(201).json({
+        ok: true,
+        name,
+        email,
+        password
     })
 }
 
-const loginUser = (req, res = response) => {
+const loginUser = (req = request, res = response) => {
+    const { email, password } = req.body;
+
+
+
     res.json({
-        ok: true
+        ok: true,
+        email,
+        password
     })
 }
 
