bfaf86029e1204b0280633865943ff8cec3b09a6
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-27 07:43:14 -0500

    Configuracion de pagina publica cuando ingrese a la ruta /

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const express = require('express');
 
 // Crear el servidor de express
+const app = express();
+
+// Directorio publico
+app.use( express.static('public') )
+
+
+// Rutas
+/*app.get('/', (req, res) => {
+    console.log('Se requiere el /')
+    res.json({
+        ok: true
+    })
+})*/
+
+
+// Escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en el puerto ${ 4000 }`)
+})
