a003fbd7779c1f8a44341dbf2fee7c5b7533be94
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-27 13:12:46 -0500

    Validacion de email and password en login

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,6 +29,7 @@ const createUser = async(req = request, res = response) => {
             name: user.name
         })
     } catch (err) {
+        console.log(err)
         res.status(500).json({
             ok: false,
             msg: 'Please talk to the administrator'
@@ -37,12 +38,43 @@ const createUser = async(req = request, res = response) => {
 
 }
 
-const loginUser = (req = request, res = response) => {
+const loginUser = async(req = request, res = response) => {
     const { email, password } = req.body;
 
-    res.status(200).json({
-        ok: true,
-    })
+    try {
+        const user = await User.findOne({ email });
+        if ( !user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User or password is incorrect - email'
+            });
+        }
+
+        // Confirm password
+        const validPassword = bcrypt.compareSync( password, user.password );
+
+        if ( !validPassword ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User or password is incorrect - password'
+            });
+        }
+
+        // Generate Jwt
+
+        return res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        });
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            ok: false,
+            msg: 'Please talk to the administrator'
+        })
+    }
 }
 
 const reNewToken = (req, res = response) => {
