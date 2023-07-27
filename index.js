a33be0adba2ac6b4ccb7bae0b031e9c1a958badb
Fredy Rivera <fredyjesus2013@gmail.com>
2023-07-27 12:21:57 -0500

    Conectandonos a mongodb con la configuracion necesaria

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { dbConnection } = require('./database/config');
 
 require('dotenv').config();
 
 // Crear el servidor de express
 const app = express();
+
+// Base de datos en mongoDB
+dbConnection()
 
 // Lectura y parseo del body
 app.use( express.json() );
@@ -15,7 +19,6 @@ app.use( express.static('public') )
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 // TODO CRUD: Eventos
-
 
 // Escuchar peticiones
 app.listen( process.env.PORT, () => {
